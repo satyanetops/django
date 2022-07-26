eba97795cbd63a451543d2d9263cd2f160796626
satyanetops <110004672+satyanetops@users.noreply.github.com>
2022-07-26 12:03:54 +0530

    Delete tests.py

--- tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class ViewsTestCase(TestCase):
-    def test_home_page_loads(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
